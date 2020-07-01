a607a3c6738d6d283c6607e5e4c51566342958df
Lenarqa <37902865+Lenarqa@users.noreply.github.com>
2020-07-01 20:52:34 +0700

    end menu and home

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const menuBtn = document.querySelector('.menu-btn');
 const hamburderBtn = document.querySelector('.menu-btn-burger');
+const nav = document.querySelector("nav")
+const menuNav = document.querySelector(".menu-nav");
+const menuNavItems = document.querySelectorAll('.menu-nav-item');
+
+
 
 let showMenu = false;
 
@@ -8,12 +13,21 @@ menuBtn.addEventListener('click', toggleMenuBtn);
 function toggleMenuBtn(){
     if(!showMenu){
         hamburderBtn.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        menuNavItems.forEach(el =>{
+            el.classList.add('open');
+        });
+        
         showMenu = true;
     }else{
         hamburderBtn.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        menuNavItems.forEach(el => {
+            el.classList.remove('open');
+        });
+
         showMenu = false;
     }
 }
-
-
-console.log(menuBtn);
